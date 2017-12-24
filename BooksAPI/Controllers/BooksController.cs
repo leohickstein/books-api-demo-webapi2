71a2df2cbbbfc11f005da298ac1622cdf5615f25
Leonardo Hickstein <leonardohickstein@hotmail.com>
2017-12-23 23:50:48 -0200

    Initial commit

---
+++ BooksAPI/Controllers/BooksController.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BooksAPI.Models;
+using System.Linq.Expressions;
+using BooksAPI.DTOs;
+
+namespace BooksAPI.Controllers
+{
+    [RoutePrefix("api/books")]
+    public class BooksController : ApiController
+    {
+        private BooksAPIContext db = new BooksAPIContext();
+
+        // Typed lambda expression for Select() method. 
+        private static readonly Expression<Func<Book, BookDto>> AsBookDto =
+            x => new BookDto
+            {
+                Title = x.Title,
+                Author = x.Author.Name,
+                Genre = x.Genre
+            };
+
+        private static readonly Expression<Func<Book, BookDetailDto>> AsBookDetailDto =
+            x => new BookDetailDto
+            {
+                Title = x.Title,
+                Genre = x.Genre,
+                PublishDate = x.PublishDate,
+                Price = x.Price,
+                Description = x.Description,
+                Author = x.Author.Name
+            };
+
+        // GET: api/books
+        [Route("")]
+        public IQueryable<BookDto> GetBooks()
+        {
+            return db.Books.Include(b => b.Author).Select(AsBookDto);
+        }
+
+        // GET: api/books/{id}
+        [Route("{id:int}")]
+        [ResponseType(typeof(BookDto))]
+        public async Task<IHttpActionResult> GetBook(int id)
+        {
+            BookDto book = await db.Books.Include(b => b.Author)
+                .Where(b => b.BookId == id)
+                .Select(AsBookDto)
+                .FirstOrDefaultAsync();
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // GET: api/books/{id}/details
+        [Route("{id:int}/details")]
+        [ResponseType(typeof(BookDetailDto))]
+        public async Task<IHttpActionResult> GetBookDetail(int id)
+        {
+            var book = await (from b in db.Books.Include(b => b.Author)
+                              where b.BookId == id
+                              select new BookDetailDto
+                              {
+                                  Title = b.Title,
+                                  Genre = b.Genre,
+                                  PublishDate = b.PublishDate,
+                                  Price = b.Price,
+                                  Description = b.Description,
+                                  Author = b.Author.Name
+                              }).FirstOrDefaultAsync();
+
+            //var book = await db.Books.Include(b => b.Author)
+            //    .Where(b => b.BookId == id)
+            //    .Select(AsBookDetailDto)
+            //    .FirstOrDefaultAsync();
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // GET: api/books/{id}/details
+        [Route("{genre}")]
+        public IQueryable<BookDto> GetBooksByGenre(string genre)
+        {
+            return db.Books.Include(b => b.Author)
+                .Where(b => b.Genre.Equals(genre, StringComparison.OrdinalIgnoreCase))
+                .Select(AsBookDto);
+        }
+
+        // GET: api/authors
+        [Route("~/api/authors")]
+        public IQueryable<Author> GetAuthors()
+        {
+            return db.Authors;
+        }
+
+        // GET: api/authors/{id}/books
+        [Route("~/api/authors/{authorid:int}/books")]
+        public IQueryable<BookDto> GetBooksByAuthor(int authorId)
+        {
+            return db.Books.Include(b => b.Author)
+                .Where(b => b.AuthorId == authorId)
+                .Select(AsBookDto);
+        }
+
+        // GET: api/books/date/yyyy-mm-dd
+        [Route("date/{pubdate:datetime:regex(\\d{4}-\\d{2}-\\d{2})}")]
+        [Route("date/{*pubdate:datetime:regex(\\d{4}/\\d{2}/\\d{2})}")]
+        public IQueryable<BookDto> GetBooksByPublicationDate(DateTime pubdate)
+        {
+            return db.Books.Include(b => b.Author)
+                .Where(b => DbFunctions.TruncateTime(b.PublishDate) == DbFunctions.TruncateTime(pubdate))
+                .Select(AsBookDto);
+        }
+
+        // DELETED FROM TUTORIAL
+        // PUT: api/Books/5
+        //[ResponseType(typeof(void))]
+        //public async Task<IHttpActionResult> PutBook(int id, Book book)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    if (id != book.BookId)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    db.Entry(book).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!BookExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        // DELETED FROM TUTORIAL
+        // POST: api/Books
+        //[ResponseType(typeof(Book))]
+        //public async Task<IHttpActionResult> PostBook(Book book)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Books.Add(book);
+        //    await db.SaveChangesAsync();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = book.BookId }, book);
+        //}
+
+        // DELETED FROM TUTORIAL
+        // DELETE: api/Books/5
+        //[ResponseType(typeof(Book))]
+        //public async Task<IHttpActionResult> DeleteBook(int id)
+        //{
+        //    Book book = await db.Books.FindAsync(id);
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.Books.Remove(book);
+        //    await db.SaveChangesAsync();
+
+        //    return Ok(book);
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        //private bool BookExists(int id)
+        //{
+        //    return db.Books.Count(e => e.BookId == id) > 0;
+        //}
+    }
+}
